923eda29b7acae8025dc89f2fc52900b11be4597
SallamRady <sallamrady@gmail.com>
2022-05-28 05:15:32 +0200

    improve project,add cart  [design/functionality]

--- ecommerce-version-1/models/Product.js
+++ ecommerce-version-1/models/Product.js
@@ -2,8 +2,24 @@ const path = require('path');
 const fs = require('fs');
 const rootPath = require('../utils/rootPath');
 
+
+const getProductsFromFile = (callback) => {
+    let dataFilePath = path.join(rootPath,'data','products.json');
+        let products = [];
+        fs.readFile(dataFilePath,
+            (err,filecontent)=>{
+                if(!err){
+                    products = JSON.parse(filecontent);
+                    return callback(products);
+                }else{
+                    return callback([]);
+                }
+            }
+        )
+}
 module.exports = class Product {
-    constructor(name,price,description){
+    constructor(id,name,price,description){
+        this.id = id;
         this.name = name;
         this.price = price;
         this.description = description;
@@ -19,24 +35,55 @@ module.exports = class Product {
                 if(!err){
                     products = JSON.parse(filecontent);
                 }
-                products.push(this);
-                fs.writeFile(dataFilePath,JSON.stringify(products),
-                (err)=>{console.log(err)});
+                if(this.id){
+                    const index = products.findIndex(p => p.id === this.id);
+                    //console.log('editttttttttt',index);
+                    if(index != -1){
+                        const existProduct = products[index];
+                        let updateProduct = {...this};
+                        products[index] = {...updateProduct};
+                        fs.writeFile(dataFilePath,JSON.stringify(products),
+                        (err)=>{console.log(err)});
+                    }
+                }else{
+                    this.id = (Math.random()*199999999).toString();
+                    products.push(this);
+                    fs.writeFile(dataFilePath,JSON.stringify(products),
+                    (err)=>{console.log(err)});
+                }
             }
         )
     }
 
-    static getAll(callback){
+
+    static delete(id){
         let dataFilePath = path.join(rootPath,'data','products.json');
         let products = [];
+
         fs.readFile(dataFilePath,
             (err,filecontent)=>{
                 if(!err){
                     products = JSON.parse(filecontent);
-                    return callback(products);
-                }else{
-                    return callback([]);
                 }
+                const index = products.findIndex(p => p.id === id);
+                
+                if(index != -1){
+                    products.splice(index,1);
+                    fs.writeFile(dataFilePath,JSON.stringify(products),
+                    (err)=>{console.log(err)});
+                }
+            }
+        )
+    }
+    static getAll(callback){
+        getProductsFromFile(callback);
+    }
+
+    static getSingleProduct(id,callback){
+        getProductsFromFile(
+            products=>{
+                const product = products.find(p=>p.id === id);
+                callback(product);
             }
         )
     }
--- ecommerce-version-1/routes/admin/AdminRoutes.js
+++ ecommerce-version-1/routes/admin/AdminRoutes.js
@@ -8,8 +8,18 @@ router.get('/add-product',AdminController.AddProduct);
 
 router.post('/save-product',AdminController.SaveProduct);
 
+router.post('/save-edit-product',AdminController.SaveEdittedProduct);
+
+router.post('/product-delete',AdminController.DeleteProduct);
+
 router.get('/product',AdminController.SingleProduct);
 
+router.get('/product/:id',AdminController.productDetails);
+
+router.get('/product-edit/:id',AdminController.productEdit);
+
 router.get('/products',AdminController.AllProducts);
+
+router.post('/cart',AdminController.addToCart);
 
 module.exports = router;
